53bd6b99cca90f7741e505c53a936f3d5b00d7f8
larry <i@goodow.com>
2014-05-07 21:34:11 +0800

    Test realtime channel features

---
+++ scripts/vertx.js
@@ -0,0 +1,6 @@
+var vertx = require('vertx')
+
+vertx.createHttpServer().requestHandler(function(req) {
+  var filename = "app/" + (req.uri() == "/" ? "index.html" : "." + req.uri());
+  req.response.sendFile(filename)
+}).listen(8000)
