650398c65d627022434112be0a270c6ae1668fec
larry <i@goodow.com>
2014-05-08 11:27:26 +0800

    git mv for gh-pages

--- scripts/vertx.js
+++
@@ -1,6 +0,0 @@
-var vertx = require('vertx')
-
-vertx.createHttpServer().requestHandler(function(req) {
-  var filename = "app/" + (req.uri() == "/" ? "index.html" : "." + req.uri());
-  req.response.sendFile(filename)
-}).listen(8000)
